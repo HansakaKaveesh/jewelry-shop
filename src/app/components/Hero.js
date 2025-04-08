2f7622fb43e2fc0ad2142efdb6995983ac296a91
HansakaKaveesh <hansakakaveesh2@gmail.com>
2025-04-08 20:09:54 +0530

    Replace hero back.mov with hero back.mp4 and update video source in Hero component

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -10,7 +10,7 @@ export default function Hero() {
       muted 
       className="absolute top-0 left-0 w-full h-full object-cover z-0"
     >
-      <source src="/hero back.mov" type="video/mp4" />
+      <source src="/hero back.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
       {/* Gradient overlay */}
